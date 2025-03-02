d6e6e5bb6a814edbf8e2202cdf69498a815fdb5e
umutsatir <u.satir2021@gtu.edu.tr>
2025-03-02 14:32:46 +0300

    empty div deleted

--- dapp/src/pages/Add/Add.tsx
+++ dapp/src/pages/Add/Add.tsx
@@ -308,20 +308,6 @@ export const Add = () => {
                             required
                         />
                     </div>
-                    <div className="flex flex-col w-3/5">
-                        <h1 className="text-xl mb-2 text-left">
-                            AI Price Prediction
-                        </h1>
-                        <button
-                            type="button"
-                            className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 w-[100px]"
-                        >
-                            Predict
-                        </button>
-                        <p className="text-2xl text-green-600">
-                            $2,000 - $4,000
-                        </p>
-                    </div>
                     <button
                         type="submit"
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
